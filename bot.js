0c2f1d262d4e7f5f5df566375e17f2a15368d480
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-12 19:01:30 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,19 +17,8 @@ API.sendChat(bot + ' Adeus,volte sempre @' + obj.username);
 }
 API.on(API.USER_LEAVE, fancyName);
 
-API.on(API.GRAB_UPDATE, callback);
+API.on(API.CHAT_COMMAND, callback);
 
-while(n == 0){
-  API.sendChat("Tocando: " + media.author + " - " + media.title);
-  ++n;
+function callback(value) {
+API.sendChat(value + ' /boom');
 }
-
-API.on(API.CHAT, function(data){
-var id = data.uid;
-var msg = data.message;
-var role = API.getUser(id).role;
-if(msg.indexOf("!cookie") == 0 && role >=0){
-API.moderateDeleteChat(data.cid);
-API.sendChat("cookie!!!!!!!");
-}
-});
