7ff6de75cf436930dadca010096da41b0782f52d
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-12 16:46:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,3 @@
-!window.pHash&&"undefined"!=typeof window.API&&API.getUser()&&!function()(){
+!window.pHash&&"undefined"!=typeof window.API&&API.getUser()&&!function(){
 API.sendChat("[NedaBot v1.0] Bot carregado com sucesso!");
 }
