7de3699da8f6eac353dd78fdeff82cc395656886
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-12 18:18:49 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 var bot = "[NedaBot]";
 var botv = "[NedaBot v1.0]";
-var media = API.getMedia();
-var id = data.uid;
-var msg = data.message;
-var role = API.getUser(id).role;
 
 API.sendChat(botv + " Bot carregado com sucesso!");
 
@@ -20,10 +16,3 @@ API.sendChat(bot + ' Adeus,volte sempre @' + obj.username);
 API.on(API.USER_LEAVE, fancyName);
 
 API.sendChat("Tocando: " + media.author + " - " + media.title);
-
-API.on(API.CHAT, function(data){
-if(msg.indexOf("!cookie") == 0 && role >=0){
-API.moderateDeleteChat(data.cid);
-API.sendChat(data.un + " COOOOKIEEEEEE");
-}
-});
