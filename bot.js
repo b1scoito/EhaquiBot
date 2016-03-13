bc25af464dfdafd47e6ee08d5a63926056792dda
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-13 14:52:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,14 +38,9 @@ if(msg.indexOf("!q-p") == 0 && role >=0){
 API.sendChat(bot + " q-q (VOCÊ) p-p");
 }
 if(msg.indexOf("!help") == 0 && role >=0){
-API.sendChat("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-API.sendChat("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 API.sendChat("[!help] -> Isso aqui que tu ta vendo");
 API.sendChat("[!q-p] -> Teu grupo aew parça");
 API.sendChat("[!cake] -> Referencia a portal..(spoiler ;-;)");
 API.sendChat("[!cookie] -> Referencia a G0uloarte e-e");
-API.sendChat("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-API.sendChat("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
 }
 });
