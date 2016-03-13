4445b99a94c44c22d4c44a863fcf57031a2fa77a
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-13 08:08:37 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ var bot = "[NedaBot]";
 var botv = "[NedaBot v1.0]";
 var nmedia = API.getNextMedia();
 var media = API.getMedia();
+var songt = API.getTimeRemaining();
 
 API.sendChat(botv + " Bot carregado com sucesso!");
 
@@ -17,5 +18,7 @@ API.sendChat(bot + ' Adeus,volte sempre @' + obj.username);
 }
 API.on(API.USER_LEAVE, fancyName);
 
-API.sendChat("Tocando: " + media.author + " - " + media.title);
-API.sendChat("Proxima musica: " + nmedia.author + " - " + nmedia.title);
+
+if(songt >=2){
+  API.sendChat("Tocando: " + media.author + " - " + media.title);
+}
