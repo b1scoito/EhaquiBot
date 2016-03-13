237f8ce4bdcb6e0430423fdd21a4736b51e095b0
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-13 11:08:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,18 @@
 var bot = "[NedaBot]";
 var botv = "[NedaBot v1.0]";
+
+
 API.sendChat(botv + " Bot carregado com sucesso!");
-var n = 0;
 
 //User join
 function fancyName(obj){
 API.sendChat(bot + ' Bem-vindo ao nosso plug! @' + obj.username);
 }
 API.on(API.USER_JOIN, fancyName);
+
+
+API.on(API.HISTORY_UPDATE, callback);
+
+function callback(a){
+  API.sendChat("Tocando: " + media.author + " - " + media.title);
+}
