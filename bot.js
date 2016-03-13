14f6f400493d849961c8ec4be0ad6f97ab853380
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-13 09:49:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,3 @@ function fancyName2(obj2){
 API.sendChat('Bem-vindo ao nosso plug! @' + obj2.username);
 }
 API.on(API.USER_JOIN, fancyName2);
-
-//User left
-function fancyName(obj){
-API.sendChat(bot + ' Adeus,volte sempre @' + obj.username);
-}
-API.on(API.USER_LEAVE, fancyName);
