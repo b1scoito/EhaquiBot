dfd64a2a59127f3cbefe210054d7b0b19efbc475
DoctorVWA <vi4-weigert@hotmail.com>
2016-03-13 11:41:30 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,3 +16,14 @@ function callback(a){
   var media = API.getMedia();
   API.sendChat(bot + "Agora tocando: " + media.author + " - " + media.title);
 }
+
+
+API.on(API.CHAT, function(data){
+var id = data.uid;
+var msg = data.message;
+var role = API.getUser(id).role;
+if(msg.indexOf("!commands") == 0 && role >=0){
+API.moderateDeleteChat(data.cid);
+API.sendChat(data.un + " has sent the command!");
+}
+});
