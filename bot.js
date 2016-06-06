cc447b42c796e59ad82767011d71251a0b74fcb2
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-06 07:49:48 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,15 +8,15 @@ API.sendChat(vbot + "Bot iniciado com sucesso!")
 API.on(API.HISTORY_UPDATE, msc)
 function msc(music){
 var media = API.getMedia();
-API.sendChat(bot + "Tocando:" + media.author + " - " + media.title)
+API.sendChat(bot + " Tocando:" + media.author + " - " + media.title)
 }
 
 API.on(API.USER_LEAVE, lvusr)
 function lvusr(userleave){
-  API.sendChat(bot + "O usuario:" +  userleave.username + " Deixou a sala!")
+  API.sendChat(bot + " O usuario:" +  userleave.username + " Deixou a sala!")
 }
 
 API.on(API.USER_JOIN, jnusr)
 function jnusr(userjoin){
-  API.sendChat(bot + "Bem-vindo(a) " + userjoin.username)
+  API.sendChat(bot + " Bem-vindo(a) " + userjoin.username)
 }
