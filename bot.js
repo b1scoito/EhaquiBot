ed371b91e196ac078171854a5ca473f277d80ff0
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-06 08:13:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,15 +20,3 @@ API.on(API.USER_JOIN, jnusr)
 function jnusr(userjoin){
   API.sendChat(bot + " Bem-vindo(a) " + userjoin.username)
 }
-
-function name(data) {
-var id = data.uid;
-var msg = data.message;
-var username = data.un;
-var role = API.getUser(id).role;
-/* method what i use to see emote,message etc. content*/
-if(msg.indexOf("!commands") == 0 && role >=0) {
-API.moderateDeleteChat(data.cid);
-API.sendChat(username +" has sent the command!");
-}}
-API.on(API.CHAT, name);
