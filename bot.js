a1a926649eb39d204f889dfb0b01dbb6773a17cd
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-06 07:08:22 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,4 @@ start = start+1
 bot = '[RAWR Bot]'
 vbot = '[RAWR Bot v1.0]'
 Author = 'GamerBolachaBR'
-if start==2 {
-  API.sendChat(vbot + "Bot iniciado com sucesso!")
-}
+API.sendChat(vbot + "Bot iniciado com sucesso!")
