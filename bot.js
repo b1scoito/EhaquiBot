0434da9c4e423c974aba6ec5c4e4a185a21a9f24
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-06 07:19:10 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,5 +2,5 @@ bot = '[RAWR Bot]'
 vbot = '[RAWR Bot v1.0]'
 Author = 'GamerBolachaBR'
 API.sendChat(vbot + "Bot iniciado com sucesso!")
-API.getMedia(author,title)
-API.sendChat(bot + "Song:" + author + " - " + title)
+var media = API.getMedia();
+API.sendChat(bot + "Song:" + media)
