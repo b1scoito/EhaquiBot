f3b173d26a2372cd24c6fb073381d9d927101bc4
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-07 08:07:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
-bot = '[RAWR Bot]'
-vbot = '[RAWR Bot v1.0]'
+bot = '[STG Bot]'
+vbot = '[STG Bot v1.0]'
 Author = 'GamerBolachaBR'
 API.sendChat(vbot + "Bot iniciado com sucesso!")
-
-
 
 API.on(API.HISTORY_UPDATE, msc)
 function msc(music){
@@ -20,3 +18,13 @@ API.on(API.USER_JOIN, jnusr)
 function jnusr(userjoin){
   API.sendChat(bot + " Bem-vindo(a) " + userjoin.username)
 }
+
+function commands(cmd) {
+  var id=cmd.uid;
+  var msg=cmd.message;
+  var perm=API.getUser(id).role;
+  if(cmd.type=="!bot" && perm >= 0){
+    API.sendChat("teste command!")
+  }
+}
+API.on(API.CHAT, commands);
