819be0de2d3ab0a232905d24ac9c2b76a8fd6a6b
DoctorVWA <vi4-weigert@hotmail.com>
2016-06-14 19:08:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,16 @@ vbot = '[STG Bot v1.0]'
 Author = 'GamerBolachaBR'
 API.sendChat(vbot + "Bot iniciado com sucesso!")
 
-API.on(API.HISTORY_UPDATE, msc)
-function msc(music){
+API.on(API.HISTORY_UPDATE,function(music){
 var media = API.getMedia();
 API.sendChat(bot + " Tocando:" + media.author + " - " + media.title)
 }
+)
 
-API.on(API.USER_LEAVE, lvusr)
-function lvusr(userleave){
+API.on(API.USER_LEAVE,function(userleave){
   API.sendChat(bot + " O usuario:" +  userleave.username + " Deixou a sala!")
 }
+)
 
 API.on(API.USER_JOIN,function(userjoin){
   API.sendChat(bot + " Bem-vindo(a) " + userjoin.username)
